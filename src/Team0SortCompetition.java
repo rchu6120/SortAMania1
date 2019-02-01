ac3261f1f08258f507c21a482ebfd505761c524b
rchu9700 <rchu9700@bths.edu>
2019-02-01 14:43:11 -0500

    fdbfgbnf

--- src/Team0SortCompetition.java
+++ src/Team0SortCompetition.java
@@ -1,28 +1,88 @@
+/**
+ * This version:
+ * @author Ricky Chu and Kiran Khambhla
+ * @version February 2019
+ */
 public class Team0SortCompetition extends SortCompetition {
 
+    @Override
     public int challengeOne(int[] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeTwo(String[] arr, String query) {
+        return 0;
+    }
+
+    @Override
+    public int challengeThree(int[] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeFour(int[][] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeFive(Comparable[] arr, Comparable query) {
+        return 0;
+    }
+
+    @Override
+    public String greeting() {
+        return "We are team zero!";
+    }
+
+    public void mergeSort(int[] arr) {
         int n = arr.length;
         int[] temp = new int[n];
         mergeSortHelper(arr, 0, n - 1, temp);
     }
 
-    public int challengeTwo(String[] arr, String query) {
-
+    public void mergeSortHelper(int[] arr, int left, int right, int[] temp) {
+        if (left < right) {
+            int mid = (left + right)/2;
+            mergeSortHelper(arr, left, mid, temp);
+            mergeSortHelper(arr, mid + 1, right, temp);
+            merge(arr, left, mid, right, temp);
+        }
     }
 
-    public int challengeThree(int[] arr) {
-
-    }
-
-    public int challengeFour(int[][] arr) {
-
-    }
-
-    public int challengeFive(Comparable[] arr, Comparable query) {
-
-    }
-
-    public String greeting() {
-
+    public int[] merge(int[] arr, int left, int mid, int right, int[] temp) {
+        int i = left;
+        int j = mid + 1;
+        int k = right;
+        int median = 0;
+        while (i <= mid && j <= right) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            }
+            else {
+                temp[k] = arr[j];
+                i++;
+            }
+            k++;
+        }
+        while (i <= mid) {
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+        while (j <= right) {
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+        for (k = left; k <= right; k++) {
+            arr[k] = temp[k];
+        }
+        for (int n = 0; n <= arr.length - 1; n++) {
+            median += arr[n];
+            n++;
+        }
+        return "Your sorted array is: " + arr + "\n" + "The median is " + median + ".";
     }
 }
