ded42bba3191ee02a08188631aef20dc4fc8e007
kiraq <kirankhambhla@yahoo.com>
2019-02-07 14:51:17 -0500

    Update SortAManiaRunner.java

--- src/SortAManiaRunner.java
+++ src/SortAManiaRunner.java
@@ -3,7 +3,8 @@ public class SortAManiaRunner {
         Team0SortCompetition team0 = new Team0SortCompetition(); //create an instance of your class
         int[] randIntArr = Team0SortCompetition.randomIntsArr(10000); //initialize  a random int array
         String[] randStringArr = Team0SortCompetition.randomStringArr(10000, 5); //initialize a random string array
-        int[] mostlySorted = Team0SortCompetition.mostlySortedBigArray(100000, mostlySortedBigArray);
+        int[] mostlySorted = Team0SortCompetition.mostlySorted();
+        thejuice[] thejuices = Team0SortCompetition.randomJuiceArr(1000);
 
         System.out.println("Challenge One");
         System.out.println("Unsorted");
@@ -34,16 +35,28 @@ public class SortAManiaRunner {
 
         System.out.println("Challenge Three");
         System.out.println("Unsorted");
-        Team0SortCompetition.printArr(mostlySorted); //print out the mostly sorted array
+        Team0SortCompetition.printArr(mostlySorted); //print out the unsorted array
 
         time = System.currentTimeMillis(); //start the timer
-        int med = team0.challengeThree(mostlySorted); //run your challenge three code
+        System.out.println("Median: "+team0.challengeThree(mostlySorted)); //run your challenge two code
         time = System.currentTimeMillis() - time; //stop the timer
         System.out.println("Sorted");
         Team0SortCompetition.printArr(mostlySorted); //check if the array was sorted correctly
 
         System.out.println("Challenge Three Time Taken: " + time * 0.001 + " Seconds"); //print the time
-        System.out.println("Median equals: " + med); //print the median you found
+        System.out.println();
+
+        System.out.println("Challenge Five");
+        System.out.println("Unsorted");
+        Team0SortCompetition.printArr(thejuices); //print out the unsorted array
+
+        time = System.currentTimeMillis(); //start the timer
+        System.out.println(": "+team0.challengeThree(mostlySorted)); //run your challenge two code
+        time = System.currentTimeMillis() - time; //stop the timer
+        System.out.println("Sorted");
+        Team0SortCompetition.printArr(thejuices); //check if the array was sorted correctly
+
+        System.out.println("Challenge Three Time Taken: " + time * 0.001 + " Seconds"); //print the time
         System.out.println();
 
     }
